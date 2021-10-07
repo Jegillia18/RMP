660cafe6f8efb195ce74f9da115dbb39733b4f39
Jessica Gilliam <Jessica18gilliam@gmail.com>
2021-10-07 16:35:29 -0400

    updated code

--- J_Codes/Import_J_Data.m
+++ J_Codes/Import_J_Data.m
@@ -66,7 +66,7 @@ for y = 1:length(set)
 %             1=HS
 %             0=TO
 %% Left Foot Start Here
-Pelvis_x=Pelvis(:,1);
+Pelvis_x = Pelvis(:,1);
 
 Left_foot_x = LeftF(:,1); %CHANGED NAME - Matches Left_Extraction original
 Left_foot_z = LeftF(:,3); %CHANGED NAME - Matches Left_Extraction original
@@ -109,20 +109,22 @@ Left_Knee = jLeftKnee(:,3);
 Left_Hip = jLeftHip(:,3);
 
 Left_HS_Time = Left_HS_Time';
-Left_HS_Time(2,1:length(Left_HS_Time)) = 1;                                %Marks HS as 1 so I can keep track of events
-Left_HS = [Left_HS_Time; Left_HS_Index];
+Left_HS_Index = Left_HS_Index';
+Left_HS_Time(1:length(Left_HS_Time)) = 1;                                %Marks HS as 1 so I can keep track of events
+Left_HS = [Left_HS_Time Left_HS_Index];
 
 
 Left_TO_Time = Left_TO_Time';
-Left_TO_Time(2,1:length(Left_TO_Time)) = 0;                                %Marks TO as 0 so I can keep track of events
-Left_TO = [Left_TO_Time; Left_TO_Index];
+Left_TO_Index = Left_TO_Index';
+Left_TO_Time(1:length(Left_TO_Time)) = 0;                                %Marks TO as 0 so I can keep track of events
+Left_TO = [Left_TO_Time Left_TO_Index];
 
 
-Left_Cycle1 = [Left_HS Left_TO];                                 %Creates an array with all the events of interests
+Left_Cycle1 = [Left_HS; Left_TO];                                 %Creates an array with all the events of interests **Stacked
+Left_Cycle_s = Left_Cycle1(:,2);
 
-
-[temp, order] = sort(Left_Cycle1(1,:) ,'ascend');                          %Sorts evetns in chronological order
-Left_Cycle2 = Left_Cycle1(:,order);                                        %Sorts evetns in chronological order                            
+[temp, order] = sort(Left_Cycle_s(:,1) ,'ascend');                          %Sorts evetns in chronological order
+Left_Cycle2 = [Left_Cycle1(order,1) temp];                                        %Sorts evetns in chronological order                            
 
 
 L_middle_25 = floor(length(Left_Cycle2)*.25);                              % finds value for 25% of detected # of events
@@ -131,49 +133,53 @@ L_middle_50 = floor(length(Left_Cycle2)*.50);                              % fin
 
 L_Start = L_middle_25+1;                                                   % Value that cuts off the first 25% adn start at the next point (+1)
 
-Left_Cycle2 = Left_Cycle2(1:end,L_Start:end);                              % Performs first 25% cut off
+Left_Cycle2 = Left_Cycle2(L_Start:end,:);                              % Performs first 25% cut off
 
-if Left_Cycle2(2,1) == 0                                                   %Checks to see if first event is TO. If yes go to next point
-    Left_Cycle2(:,1) = [];
+% if Left_Cycle2(1,1) == 0                                                   %Checks to see if first event is TO. If yes go to next point
+%     Left_Cycle2(1,:) = [];
+% end
+
+Left_Cycle = Left_Cycle2(1:L_middle_50,:);                             %Pulls data starting at new first point through the amount of points that is equal to 50% of the # number of events detected.
+
+%Left_Angle_Event_Finder = Left_Cycle(3,1:end)+1;             % need to add 1 when looking into the joint data to make up for miss alignment of indexes when it is pulled into a single variable
+
+
+
+% What if we did Left ankle and just used cycle
+Left_Ankle_Angle = Left_Cycle(:,2);
+%Left_Peak_Dorsi = max(Left_Ankle_Angle);
+
+% Left_Ankle_Event = Left_Ankle(Left_Angle_Event_Finder);                 %Ankle angle at toe off and IC from Left Cycle variable
+% 
+% Left_Knee_Event = Left_Knee(Left_Angle_Event_Finder);                   %Knee angle at toe off and IC from Left Cycle variable                 
+% 
+% Left_Hip_Event = Left_Hip(Left_Angle_Event_Finder);                     %Hip angle at toe off and IC from Left Cycle variable
+
+%Left_Peak_Dorsi = [];
+
+for i=2:2:length(Left_Ankle_Angle)
+Left_Peak_Ankle = max(Left_Ankle(Left_Ankle_Angle(i-1):Left_Ankle_Angle(i)));
+
+data.(['Participant_',num2str(p),]).(['Trial_', num2str(t),]).(['WALK_', num2str(d),]).L(i,:) = Left_Peak_Ankle;
+%i = i + 2;
 end
 
-Left_Cycle = Left_Cycle2(1:end,1:L_middle_50);                             %Pulls data starting at new first point through the amount of points that is equal to 50% of the # number of events detected.
-
-Left_Angle_Event_Finder = Left_Cycle(3,1:end)+1;             % need to add 1 when looking into the joint data to make up for miss alignment of indexes when it is pulled into a single variable
+% NA_Fill = [nan; nan; nan]; %blank array to use as a fill for the inbetween variables that will be one less then the number of steps
 
 
 
+% LPA_Knee = Left_Knee(LPA_I);                                              %knee angle at peak ankle
+% LPA_Hip = Left_Hip(LPA_I);                                                %hip angle at peak ankle
+% Left_Peak_Ankle = (Left_Peak_Ankle);%; LPA_Knee'; LPA_Hip'];                %combining ankle, knee, and hip angle at Left peak ankle to one variable
+%Left_Peak_Ankle = [Left_Peak_Ankle NA_Fill]';                             %adding the fill variable to even out arrays
 
-Left_Ankle_Event = Left_Ankle(Left_Angle_Event_Finder);                 %Ankle angle at toe off and IC from Left Cycle variable
-
-Left_Knee_Event = Left_Knee(Left_Angle_Event_Finder);                   %Knee angle at toe off and IC from Left Cycle variable                 
-
-Left_Hip_Event = Left_Hip(Left_Angle_Event_Finder);                     %Hip angle at toe off and IC from Left Cycle variable
-                   
-
-for i=1:length(Left_Angle_Event_Finder)-1
-[Left_Peak_Ankle(i) LPA_I(i)] = max(Left_Ankle(Left_Angle_Event_Finder(i):Left_Angle_Event_Finder(i+1)));
-
-[Left_Peak_Knee(i) LKA_I(i)] = max(Left_Knee(Left_Angle_Event_Finder(i):Left_Angle_Event_Finder(i+1)));
-
-end
-
-NA_Fill = [nan; nan; nan]; %blank array to use as a fill for the inbetween variables that will be one less then the number of steps
-
-
-
-LPA_Knee = Left_Knee(LPA_I);                                              %knee angle at peak ankle
-LPA_Hip = Left_Hip(LPA_I);                                                %hip angle at peak ankle
-Left_Peak_Ankle = [Left_Peak_Ankle; LPA_Knee'; LPA_Hip'];                %combining ankle, knee, and hip angle at Left peak ankle to one variable
-Left_Peak_Ankle = [Left_Peak_Ankle NA_Fill];                             %adding the fill variable to even out arrays
-
-LKA_Ankle = Left_Ankle(LKA_I);                                            %ankle angle at peak knee
-LKA_Hip = Left_Hip(LKA_I);                                                %hip angle at peak knee
-Left_Peak_Knee = [Left_Peak_Knee; LKA_Ankle'; LKA_Hip'];        %combining ankle, knee, and hip angle at Left peak knee to one variable
-Left_Peak_Knee = [Left_Peak_Knee NA_Fill];                               %adding the fill variable to even out arrays
+% LKA_Ankle = Left_Ankle(LKA_I);                                            %ankle angle at peak knee
+% LKA_Hip = Left_Hip(LKA_I);                                                %hip angle at peak knee
+% Left_Peak_Knee = [Left_Peak_Knee; LKA_Ankle'; LKA_Hip'];        %combining ankle, knee, and hip angle at Left peak knee to one variable
+% Left_Peak_Knee = [Left_Peak_Knee NA_Fill];                               %adding the fill variable to even out arrays
 
 % 
-Left = [Left_Cycle ; Left_Ankle_Event'; Left_Knee_Event'; Left_Hip_Event'; Left_Peak_Ankle ; Left_Peak_Knee];
+%Left = [Left_Cycle ; Left_Ankle_Event'; Left_Knee_Event'; Left_Hip_Event'; Left_Peak_Ankle ; Left_Peak_Knee];
                
                
 %% Right Foot Start Here
@@ -218,77 +224,77 @@ Right_Knee = jRightKnee(:,3);
 Right_Hip = jRightHip(:,3);
 
 
-
 Right_HS_Time = Right_HS_Time';
-Right_HS_Time(2,1:length(Right_HS_Time)) = 1;                              %Marks HS as 1 so I can keep track of events
-Right_HS = [Right_HS_Time; Right_HS_Index];
+Right_HS_Index = Right_HS_Index';
+Right_HS_Time(1:length(Right_HS_Time)) = 1;                                %Marks HS as 1 so I can keep track of events
+Right_HS = [Right_HS_Time Right_HS_Index];
+
 
 Right_TO_Time = Right_TO_Time';
-Right_TO_Time(2,1:length(Right_TO_Time)) = 0;                              %Marks TO as 0 so I can keep track of events
-Right_TO = [Right_TO_Time; Right_TO_Index];
-
-Right_Cycle1 = [Right_HS Right_TO];                               %Creates an array with all the events of interests
-
-
-[temp, order] = sort(Right_Cycle1(1,:) ,'ascend');                         %Sorts evetns in chronological order
-Right_Cycle2 = Right_Cycle1(:,order);                                      %Sorts evetns in chronological order                            
+Right_TO_Index = Right_TO_Index';
+Right_TO_Time(1:length(Right_TO_Time)) = 0;                                %Marks TO as 0 so I can keep track of events
+Right_TO = [Right_TO_Time Right_TO_Index];
 
 
-R_middle_25 = floor(length(Right_Cycle2)*.25);                             % finds value for 25% of detected # of events
-R_middle_50 = floor(length(Right_Cycle2)*.50);                             % finds value for 50% of detected # of events
+Right_Cycle1 = [Right_HS; Right_TO];                                 %Creates an array with all the events of interests **Stacked
+Right_Cycle_s = Right_Cycle1(:,2);
+
+[temp, order] = sort(Right_Cycle_s(:,1) ,'ascend');                          %Sorts evetns in chronological order
+Right_Cycle2 = [Right_Cycle1(order,1) temp];                                        %Sorts evetns in chronological order                            
+
+
+R_middle_25 = floor(length(Right_Cycle2)*.25);                              % finds value for 25% of detected # of events
+R_middle_50 = floor(length(Right_Cycle2)*.50);                              % finds value for 50% of detected # of events
 
 
 R_Start = R_middle_25+1;                                                   % Value that cuts off the first 25% adn start at the next point (+1)
 
-Right_Cycle2 = Right_Cycle2(1:end,R_Start:end);                            % Performs first 25% cut off
+Right_Cycle2 = Right_Cycle2(L_Start:end,:);                              % Performs first 25% cut off
 
-if Right_Cycle2(2,1) == 0                                                  %Checks to see if first event is TO. If yes go to next point
-    Right_Cycle2(:,1) = [];
+if Right_Cycle2(1,1) == 0                                                   %Checks to see if first event is TO. If yes go to next point
+    Right_Cycle2(1,:) = [];
 end
 
-Right_Cycle = Right_Cycle2(1:end,1:R_middle_50);                           %Pulls data starting at new first point through the amount of points that is equal to 50% of the # number of events detected.
+Right_Cycle = Right_Cycle2(1:L_middle_50,:);                             %Pulls data starting at new first point through the amount of points that is equal to 50% of the # number of events detected.
 
-Right_Angle_Event_Finder = Right_Cycle(3,1:end)+1;     %need index to find in between values        % need to add 1 when looking into the joint data to make up for miss alignment of indexes when it is pulled into a single variable
+Right_Ankle_Angle = Right_Cycle(:,2);
+%Left_Peak_Dorsi = max(Left_Ankle_Angle);
 
+% Left_Ankle_Event = Left_Ankle(Left_Angle_Event_Finder);                 %Ankle angle at toe off and IC from Left Cycle variable
+% 
+% Left_Knee_Event = Left_Knee(Left_Angle_Event_Finder);                   %Knee angle at toe off and IC from Left Cycle variable                 
+% 
+% Left_Hip_Event = Left_Hip(Left_Angle_Event_Finder);                     %Hip angle at toe off and IC from Left Cycle variable
 
-Right_Ankle_Event = Right_Ankle(Right_Angle_Event_Finder);                 %Ankle angle at toe off and IC from Right Cycle variable
+%Left_Peak_Dorsi = [];
 
-Right_Knee_Event = Right_Knee(Right_Angle_Event_Finder);                   %Knee angle at toe off and IC from Right Cycle variable                 
+for i=2:2:length(Right_Ankle_Angle)
+Right_Peak_Ankle = max(Right_Ankle(Right_Ankle_Angle(i-1):Right_Ankle_Angle(i)));
 
-Right_Hip_Event = Right_Hip(Right_Angle_Event_Finder);                     %Hip angle at toe off and IC from Right Cycle variable
-                   
-
-for i=1:length(Right_Angle_Event_Finder)-1
-[Right_Peak_Ankle(i) RPA_I(i)] = max(Right_Ankle(Right_Angle_Event_Finder(i):Right_Angle_Event_Finder(i+1)));
-
-[Right_Peak_Knee(i) RKA_I(i)] = max(Right_Knee(Right_Angle_Event_Finder(i):Right_Angle_Event_Finder(i+1)));
-
+data.(['Participant_',num2str(p),]).(['Trial_', num2str(t),]).(['WALK_', num2str(d),]).R(i,:) = Right_Peak_Ankle;
+%i = i + 2;
 end
 
-NA_Fill = [nan; nan; nan]; %blank array to use as a fill for the inbetween variables that will be one less then the number of steps
+% RPA_Knee = Right_Knee(RPA_I);                                              %knee angle at peak ankle
+% RPA_Hip = Right_Hip(RPA_I);                                                %hip angle at peak ankle
+% Right_Peak_Ankle = (Right_Peak_Ankle);%; RPA_Knee'; RPA_Hip'];                %combining ankle, knee, and hip angle at right peak ankle to one variable
+% %Right_Peak_Ankle = [Right_Peak_Ankle NA_Fill]';                             %adding the fill variable to even out arrays
+% 
+% RKA_Ankle = Right_Ankle(RKA_I);                                            %ankle angle at peak knee
+% RKA_Hip = Right_Hip(RKA_I);                                                %hip angle at peak knee
+% Right_Peak_Knee = [Right_Peak_Knee; RKA_Ankle'; RKA_Hip';];        %combining ankle, knee, and hip angle at right peak knee to one variable
+% Right_Peak_Knee = [Right_Peak_Knee NA_Fill];                               %adding the fill variable to even out arrays
 
 
-
-RPA_Knee = Right_Knee(RPA_I);                                              %knee angle at peak ankle
-RPA_Hip = Right_Hip(RPA_I);                                                %hip angle at peak ankle
-Right_Peak_Ankle = [Right_Peak_Ankle; RPA_Knee'; RPA_Hip'];                %combining ankle, knee, and hip angle at right peak ankle to one variable
-Right_Peak_Ankle = [Right_Peak_Ankle NA_Fill];                             %adding the fill variable to even out arrays
-
-RKA_Ankle = Right_Ankle(RKA_I);                                            %ankle angle at peak knee
-RKA_Hip = Right_Hip(RKA_I);                                                %hip angle at peak knee
-Right_Peak_Knee = [Right_Peak_Knee; RKA_Ankle'; RKA_Hip';];        %combining ankle, knee, and hip angle at right peak knee to one variable
-Right_Peak_Knee = [Right_Peak_Knee NA_Fill];                               %adding the fill variable to even out arrays
-
-
-Right = [Right_Cycle ; Right_Ankle_Event'; Right_Knee_Event'; Right_Hip_Event'; Right_Peak_Ankle ; Right_Peak_Knee];
+%Right = [Right_Cycle ; Right_Ankle_Event'; Right_Knee_Event'; Right_Hip_Event'; Right_Peak_Ankle ; Right_Peak_Knee];
 
 
 %% Data Saved Here: Dorsiflexion angle during swing phase for middle 50%
 
 
-data.(['Participant_',num2str(p),]).(['Trial_', num2str(t),]).(['WALK_', num2str(d),]).L = table(Left_Peak_Ankle); 
-
-data.(['Participant_',num2str(p),]).(['Trial_', num2str(t),]).(['WALK_', num2str(d),]).R = table(Right_Peak_Ankle);
+% data.(['Participant_',num2str(p),]).(['Trial_', num2str(t),]).(['WALK_', num2str(d),]).L = table(Left_Peak_Dorsi); 
+% 
+% data.(['Participant_',num2str(p),]).(['Trial_', num2str(t),]).(['WALK_', num2str(d),]).R = table(Right_Peak_Dorsi);
 
 
 clearvars -except p t set d y data 
